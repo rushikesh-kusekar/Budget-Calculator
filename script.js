5e05c0d982f2b574f05a270d56d89c72bedb4006
rushikesh-kusekar <kusekarrushikesh45@gmail.com>
2024-11-10 13:19:20 +0530

    scipt added to modify list element

--- script.js
+++ script.js
@@ -40,4 +40,34 @@ totalAmountButton.addEventListener("click",() =>{
     }
 });
 
+//function to disable edit and delete button
 
+
+const disableButtons = (bool) =>{
+    let editButtons = document.getElementsByClassName("edit");
+
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool ;
+    });
+};
+
+//function to modify list elements
+
+const modifyElements = (element,edit = false) =>{
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue .innerText ;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+
+    if(edit){
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    }
+
+    balanceValue.innerText =parseInt(currentBalance) +  parseInt(parentAmount);
+    expenditureValue.innerText =parseInt(currentExpense)-parseInt(parentAmount);
+
+    parentDiv.remove();
+}
