925fb390d9cc9562b567183e0037b26ed7dafe4b
rushikesh-kusekar <kusekarrushikesh45@gmail.com>
2024-11-10 12:52:58 +0530

    total budget set button script added

---
+++ script.js
@@ -0,0 +1,43 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount =document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+
+let tempAmount = 0;
+
+//set budget
+
+totalAmountButton.addEventListener("click",() =>{
+
+    tempAmount = totalAmount.value;
+    
+    //empty or negative error
+
+    if(tempAmount === " " || tempAmount < 0){
+        errorMessage.classList.remove("hide");
+       
+    }else{
+       
+        errorMessage.classList.add("hide");
+
+        //set budget
+        amount.innerHTML = tempAmount;
+
+        //set balance
+        balanceValue.innerText = tempAmount - expenditureValue.innerText;
+        
+        //clear input box
+        totalAmount.value = "" ;
+        
+    }
+});
+
+
